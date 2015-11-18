132e07c47eb8741f5e6ab0826454b878a2521fd3
Ali Mahmoud <Ali@dhcp037-053-085.wireless.buffalo.edu>
2015-11-17 23:22:30 -0500

    hovering over a show now highlights the show's time

--- schedule/schedule.js
+++ schedule/schedule.js
@@ -1,8 +1,34 @@
 $(document).ready(function() {
+  function isEmpty( el ){
+     return !$.trim(el.html())
+  }
+
   $( "td" ).mouseover(function() {
     $(this).addClass('hover');
+    $(this).parent().find('td:first').addClass('hover');
+    if($(this).attr('rowspan')>=2){
+      $(this).closest('tr').next().find('td:first').addClass('hover');
+    }
+    if($(this).attr('rowspan')>=3){
+      $(this).closest('tr').next().next().find('td:first').addClass('hover');
+    }
+    if($(this).attr('rowspan')>=4){
+      $(this).closest('tr').next().next().next().find('td:first').addClass('hover');
+    }
   });
+
   $( "td" ).mouseout(function() {
-    $(this).removeClass('hover');
+  $(this).removeClass('hover');
+  $(this).css('cursor', 'default');
+  $(this).parent().find('td:first').removeClass('hover');
+  if($(this).attr('rowspan')>=2){
+    $(this).closest('tr').next().find('td:first').removeClass('hover');
+  }
+  if($(this).attr('rowspan')>=3){
+    $(this).closest('tr').next().next().find('td:first').removeClass('hover');
+  }
+  if($(this).attr('rowspan')>=4){
+    $(this).closest('tr').next().next().next().find('td:first').removeClass('hover');
+  }
   });
 });
